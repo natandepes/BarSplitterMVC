0e45f0918a0b415906a43c43aefa382b0369a96e
Natan Depes <103089417+natandepes@users.noreply.github.com>
2023-11-13 19:50:45 -0300

    Revert "Edição de diagrama de classes"

--- Program.cs
+++ Program.cs
@@ -15,9 +15,13 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
 app.UseRouting();
+
 app.UseAuthorization();
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
 app.Run();
