c8e7c5e62f5ebda654d7210654efdcd135ded69f
Alexandre Maia <filhoale@hotmail.com>
2023-11-25 17:51:55 -0300

    Usuario e Grupo

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using BarSplitterMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BarSplitterMVC.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public UsuarioController() { 
+        }
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,55 @@
+﻿namespace BarSplitterMVC.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public bool Admin { get; set; }
+        public bool CriadorMesa { get; set; }
+        public bool ParicipaJogo { get; set; }
+        public decimal Saldo { get; set; } // Adicionei saldo para conseguir fazer o médoto pagar 
+
+        public List<Item> Itens { get; set; } = new List<Item>();  // armazenar itens do usuário
+
+
+        public Usuario(int id, string nome, bool admin, bool criadorMesa, bool participaJogo, decimal saldo)
+        {
+            Id = id;
+            Nome = nome;
+            Admin = admin;
+            CriadorMesa = criadorMesa;
+            ParicipaJogo = participaJogo;
+            Saldo = saldo;
+        }
+
+        public void PagarUsuario(decimal valor, Usuario quemRecebe)
+        {
+            if (valor > 0 && valor <= Saldo)
+            {
+                Saldo -= valor;
+                quemRecebe.Saldo += valor;
+
+                Console.WriteLine($"Pagamento de {valor:C} realizado de {Nome} para {quemRecebe.Nome}.");
+            }
+            else
+            {
+                Console.WriteLine("Erro");
+            }
+        }
+
+        public void AdicionarItem(Item item)
+        {
+            Itens.Add(item);
+        }
+
+        public void RemoverItem(Item item)
+        {
+            Itens.Remove(item);
+        }
+
+        //public void ParticiparJogo(Jogo jogo)
+        //{
+        //    jogo.AdicionarParticipante(this);
+        //}
+    }
+}
