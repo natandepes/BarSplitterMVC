4b74043b27763e6d47808567f7bab5aad0f0e199
Lucas André Imbroisi <103373950+lucasaimb@users.noreply.github.com>
2023-11-29 23:18:50 -0300

    A

---
+++ Models/Grupo.cs
@@ -0,0 +1,9 @@
+﻿namespace BarSplitterMVC.Models
+{
+    public class Grupo
+    {
+        public string nome { get; set; }
+        public List<Usuario> participantesSub { get; set; }
+        public Usuario MyProperty { get; set; }
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿namespace BarSplitterMVC.Models
+{
+    public class Usuario
+    {
+        public string nome { get; set; }
+        public int id { get; set; }
+        public List<ItemPedido> Itens { get; set; }
+        public bool participaJogo { get; set; }
+        public bool admin { get; set; }
+
+
+    }
+}
