8a40d4990ba7921da6c50a3f8c69b76c54368051
Natan <natandepes@hotmail.com>
2023-11-29 23:14:02 -0300

    Acabando o model da mesa e subindo a branch DEV.

--- Models/Mesa.cs
+++ Models/Mesa.cs
@@ -5,6 +5,11 @@ namespace BarSplitterMVC.Models
 	{
 		public string Id { get; set; }
 		public double ValorTotal { get; set; }
-	}
+		public Usuario UsuarioAdmin { get; set; }
+        public List<Usuario> Usuarios { get; set; }
+        public List<Grupo> Grupos { get; set; }
+        public List<Item> Itens { get; set; }
+
+    }
 }
 
