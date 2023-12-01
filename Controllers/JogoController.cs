182bfea8bb83535deda2abd3ef8ebe8692be3dd9
Natan <natandepes@hotmail.com>
2023-11-30 21:21:47 -0300

    Inicio do Controller Jogo, navegação funcionando e estrutura básica dele.

---
+++ Controllers/JogoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BarSplitterMVC.Controllers
+{
+    public class JogoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
