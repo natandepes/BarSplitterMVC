38cadbb5a411875da4a6b94152f1992329f3de17
Natan <natandepes@hotmail.com>
2023-11-30 21:30:22 -0300

    Subindo a branch Jogo

--- Models/Jogo.cs
+++ Models/Jogo.cs
@@ -3,6 +3,7 @@
     public class Jogo
     {
         public Item Prenda { get; set; }
-        public List<Rodada> Rodadas { get; set; }
+        public List<Usuario> Usuarios { get; set; } = new List<Usuario>();
+        public List<Rodada> Rodadas { get; set; } = new List<Rodada> ();
     }
 }
