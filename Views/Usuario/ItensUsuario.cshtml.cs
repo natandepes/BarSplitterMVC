d661d2ca597878d14944f436531262be871b5cc0
Lucas André Imbroisi <103373950+lucasaimb@users.noreply.github.com>
2023-12-03 21:56:01 -0300

    Configs novas

---
+++ Views/Usuario/ItensUsuario.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BarSplitterMVC.Views.Usuario
+{
+    public class ItensUsuarioModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
